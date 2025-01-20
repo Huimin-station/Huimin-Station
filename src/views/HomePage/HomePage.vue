deeaadfcf59a5ab1abc471cdb576d985a107c78f
Huimin-station <3434155042@qq.com>
2025-01-20 08:59:31 +0800

    首页的优化调整

--- src/views/HomePage/HomePage.vue
+++ src/views/HomePage/HomePage.vue
@@ -3,16 +3,21 @@ import HeaderIndex from '@/components/header/headerIndex.vue'
 import FooterIndex from '@/components/footer/footerIndex.vue'
 import GoodBigBox from '@/components/Goods/GoodBigBox.vue';
 import CardIndex from '@/components/temp-card/CardIndex.vue'
+
+import BoderSet from '@/components/others/BoderSet.vue';
 </script>
 
 <template>
   <div>
     <header-index></header-index>
     <GoodBigBox></GoodBigBox>
+    <BoderSet></BoderSet>
     <card-index ></card-index>
+    <BoderSet></BoderSet>
     <footer-index></footer-index>
   </div>
 </template>
 
 <style scoped>
+
 </style>
